a34e67e30b7422f3d7039b8d93f8547bab797328
Eldar <eldar219@gmail.com>
2020-08-29 17:45:35 +0400

    Added image widget with Centered

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  String name = 'Eldar Najafov';
-  int a = 34;
-  int b = 10;
-  int c = a + b;
-  String txtMessage = '';
-
-  if (c > 60) {
-    txtMessage = 'Value overlimit';
-  } else {
-    txtMessage = 'Value is ok';
-  }
-  runApp(MaterialApp(
-    home: Center(
-      child: Text(
-        'Hello, $name! \nHow re you? \n$txtMessage',
-        overflow: TextOverflow.ellipsis,
-        style: TextStyle(fontWeight: FontWeight.normal, fontSize: 30),
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[100],
+        appBar: AppBar(
+          title: Text("I Am Rich"),
+          backgroundColor: Colors.indigo[700],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                "https://images.squarespace-cdn.com/content/v1/5237604ce4b0e51f969029ae/1524415505794-237TGNOPQIYFAW3LWJGY/ke17ZwdGBToddI8pDm48kFTEgwhRQcX9r3XtU0e50sUUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcW7uEhC96WQdj-SwE5EpM0lAopPba9ZX3O0oeNTVSRxdHAmtcci_6bmVLoSDQq_pb/i-am-rich.jpg"),
+          ),
+        ),
       ),
     ),
-  ));
+  );
 }
